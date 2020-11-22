941469ddf4acccfb39c90a017e421a001b12a907
Marty Warfel <marthawarfel@gmail.com>
2020-11-22 21:45:58 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,9 +1,9 @@
 def mario
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase
+  phrase = "It's-a me, Mario!"
 end
 
-phrase = "It's-a me, Mario'"
 
 def toadstool
   puts status
